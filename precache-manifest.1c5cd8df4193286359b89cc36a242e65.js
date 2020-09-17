1515ef77aeed62c5272eee0ff0d29720220302ee
Bea P. Santana <beapsant@gmail.com>
2020-09-17 11:28:47 +0100

    Updates

---
+++ precache-manifest.1c5cd8df4193286359b89cc36a242e65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54a265d8f0385c945f91f368cfd9678c",
+    "url": "/rick-and-morty/index.html"
+  },
+  {
+    "revision": "83d54d44b3cdd6fc5ff5",
+    "url": "/rick-and-morty/static/css/main.85d17d76.chunk.css"
+  },
+  {
+    "revision": "f17e11aa4f4128dadb19",
+    "url": "/rick-and-morty/static/js/2.dcd6e1d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rick-and-morty/static/js/2.dcd6e1d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83d54d44b3cdd6fc5ff5",
+    "url": "/rick-and-morty/static/js/main.0d37813c.chunk.js"
+  },
+  {
+    "revision": "8ec831075791f93706ea",
+    "url": "/rick-and-morty/static/js/runtime-main.2b22dfe8.js"
+  },
+  {
+    "revision": "a4a6f4308dd6d2e8ea4dc92308fe07b1",
+    "url": "/rick-and-morty/static/media/search.a4a6f430.svg"
+  }
+]);
